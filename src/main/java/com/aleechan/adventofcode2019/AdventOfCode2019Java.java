20907063a9c1b8044dea74ab945021dc2180efbd
aleechan <firefly1585@gmail.com>
2019-12-03 22:55:17 -0500

    initial commit

---
+++ src/main/java/com/aleechan/adventofcode2019/AdventOfCode2019Java.java
@@ -0,0 +1,10 @@
+package com.aleechan.adventofcode2019;
+
+import java.util.Arrays;
+
+public class AdventOfCode2019Java {
+
+    public static void main(String[] args) {
+        Arrays.stream(args).forEach(arg -> System.out.println(arg));
+    }
+}
